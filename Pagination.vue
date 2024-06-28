ae262f90c00713842a66a406edb19a4c5b2746f8
Little star <17875728393@163.com>
2024-06-29 03:20:39 +0800

    refactor: 重构代码

--- Pagination.vue
+++ Pagination.vue
@@ -1,24 +1,20 @@
 <template>
   <div class="container">
     <div class="prev item" @click="prevClick">&lt;</div>
-    <div class="first item" @click="firstClick" :class="{ active: activeIndex === firstData }">
-      {{ firstData }}
-    </div>
-    <div class="more item" @click="prevMoreClick" v-show="startIndex > 0">
-      ...
-    </div>
+    <div class="first item" @click="firstClick" :class="{ active: activeIndex === firstData }">{{ firstData }}</div>
+    <div class="more item" @click="prevMoreClick" v-show="startIndex > 0">...</div>
     <div class="list" @click="clickTarget" ref="list_dom">
-      <div class="item" v-for="page in visiblePage" :key="page" :style="{ width: itemMaxWidth(page) }"
-        :class="{ active: activeIndex === page }">
+      <div
+        class="item"
+        v-for="page in visiblePage"
+        :key="page"
+        :class="{ active: activeIndex === page }"
+      >
         {{ page }}
       </div>
     </div>
-    <div class="more item" @click="nextMoreClick" v-show="endIndex < datasLength">
-      ...
-    </div>
-    <div class="last item" @click="lastClick" :class="{ active: activeIndex === lastData }">
-      {{ lastData }}
-    </div>
+    <div class="more item" @click="nextMoreClick" v-show="endIndex < datas.length">...</div>
+    <div class="last item" @click="lastClick" :class="{ active: activeIndex === lastData }">{{ lastData }}</div>
     <div class="next item" @click="nextClick">&gt;</div>
   </div>
 </template>
@@ -38,7 +34,9 @@ const props = defineProps({
 });
 const emits = defineEmits(["currentPage"]);
 
-const datas = new Array(Math.ceil(props.pageCount)).fill(1).map((item, index) => item + index);
+const datas = new Array(Math.ceil(props.pageCount))
+  .fill(1)
+  .map((item, index) => item + index);
 
 let visibleCount: number = 0;
 let screenSize: number = 0;
@@ -46,7 +44,6 @@ let screenSize: number = 0;
 const itemSize: number = 30;
 const lastData: number = datas.pop();
 const firstData: number = datas.shift();
-const datasLength: number = datas.length;
 
 const activeIndex: Ref<number> = ref(0);
 const startIndex: Ref<number> = ref(0);
@@ -57,67 +54,71 @@ const list_dom = ref<HTMLElement | null>(null);
 const visiblePage = computed(() => datas.slice(startIndex.value, endIndex.value));
 
 const prevClick = () => {
-  activeIndex.value = Math.max(firstData, activeIndex.value - 1);
-  if (activeIndex.value > visibleCount) {
-    if (activeIndex.value <= datasLength - visibleCount + 2) {
-      startIndex.value--;
-    }
-  } else {
-    startIndex.value = 0;
+  const currentIndex = activeIndex.value - 1;
+  if (currentIndex >= firstData) {
+    startIndex.value =
+      currentIndex > visibleCount
+        ? currentIndex <= datas.length - visibleCount + 2
+          ? startIndex.value - 1
+          : startIndex.value
+        : 0;
+
+    endIndex.value = startIndex.value + visibleCount;
+    activeIndex.value = currentIndex;
+    emits("currentPage", activeIndex.value);
   }
-  endIndex.value = startIndex.value + visibleCount;
-  emits('currentPage', activeIndex.value);
 };
 
 const nextClick = () => {
-  activeIndex.value = Math.min(lastData, activeIndex.value + 1);
-  if (activeIndex.value <= datasLength - visibleCount + 2) {
-    if (activeIndex.value > visibleCount) {
-      startIndex.value++;
-    }
-  } else {
-    startIndex.value = datasLength - visibleCount;
+  const currentIndex = activeIndex.value + 1;
+  if (currentIndex <= lastData) {
+    endIndex.value =
+      currentIndex <= datas.length - visibleCount + 2
+        ? currentIndex > visibleCount
+          ? endIndex.value + 1
+          : endIndex.value
+        : datas.length;
+
+    startIndex.value = endIndex.value - visibleCount;
+    activeIndex.value = currentIndex;
+    emits("currentPage", activeIndex.value);
   }
-  endIndex.value = startIndex.value + visibleCount;
-  emits('currentPage', activeIndex.value);
 };
 
 const clickTarget = (event: MouseEvent) => {
   const target = event.target as HTMLElement;
   if (target !== list_dom.value) {
     activeIndex.value = Number(target.textContent);
-    emits('currentPage', activeIndex.value);
+    emits("currentPage", activeIndex.value);
   }
 };
 
 const prevMoreClick = () => {
-  activeIndex.value -= activeIndex.value === lastData ? visibleCount + 1 : visibleCount;
-  activeIndex.value = Math.max(firstData, activeIndex.value);
   startIndex.value = Math.max(0, startIndex.value - visibleCount);
   endIndex.value = startIndex.value + visibleCount;
-  emits('currentPage', activeIndex.value);
+  activeIndex.value = endIndex.value + 1;
+  emits("currentPage", activeIndex.value);
 };
 
 const nextMoreClick = () => {
-  activeIndex.value += activeIndex.value === firstData ? visibleCount + 1 : visibleCount;
-  activeIndex.value = Math.min(lastData, activeIndex.value);
-  startIndex.value = Math.min(endIndex.value, datasLength - visibleCount);
+  startIndex.value = Math.min(endIndex.value, datas.length - visibleCount);
   endIndex.value = startIndex.value + visibleCount;
-  emits('currentPage', activeIndex.value);
+  activeIndex.value = startIndex.value + 2;
+  emits("currentPage", activeIndex.value);
 };
 
 const firstClick = () => {
   startIndex.value = 0;
   endIndex.value = startIndex.value + visibleCount;
   activeIndex.value = firstData;
-  emits('currentPage', activeIndex.value);
+  emits("currentPage", activeIndex.value);
 };
 
 const lastClick = () => {
-  startIndex.value = datasLength - visibleCount;
+  startIndex.value = datas.length - visibleCount;
   endIndex.value = startIndex.value + visibleCount;
   activeIndex.value = lastData;
-  emits('currentPage', activeIndex.value);
+  emits("currentPage", activeIndex.value);
 };
 
 onMounted(() => {
@@ -127,13 +128,9 @@ onMounted(() => {
     startIndex.value = 0;
     endIndex.value = startIndex.value + visibleCount;
     activeIndex.value = firstData;
-    emits('currentPage', activeIndex.value);
+    emits("currentPage", activeIndex.value);
   }
 });
-
-function itemMaxWidth(page: number) {
-  return page > 999 ? "auto" : itemSize;
-}
 </script>
 
 <style scoped>
@@ -142,7 +139,7 @@ function itemMaxWidth(page: number) {
   justify-content: center;
   align-items: center;
   column-gap: 5px;
-  width: 100%;
+  width: auto;
   height: 30px;
   font-size: 14px;
 }
@@ -161,7 +158,8 @@ function itemMaxWidth(page: number) {
   justify-content: center;
   align-items: center;
   padding: 0 4px;
-  width: 30px;
+  width: auto;
+  min-width: 30px;
   height: inherit;
   line-height: 30px;
   cursor: pointer;
