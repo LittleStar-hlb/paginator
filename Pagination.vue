7ad6509ffaf70990c9a672263779c568b2fc748b
Little star <17875728393@163.com>
2024-06-29 18:51:18 +0800

    feat: 添加搜索页码功能

--- Pagination.vue
+++ Pagination.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="container">
-    <div class="prev item" @click="prevClick">&lt;</div>
-    <div class="first item" @click="firstClick" :class="{ active: activePage === firstData }">{{ firstData }}</div>
-    <div class="more item" @click="prevMoreClick" v-show="startIndex > 0">...</div>
+    <div class="prev item" @click="prevEvent">&lt;</div>
+    <div class="first item" @click="firstEvent" :class="{ active: activePage === firstData }">{{ firstData }}</div>
+    <div class="more item" @click="prevMoreEvent" v-show="startIndex > 0">...</div>
     <div class="list" @click="clickTarget">
       <div
           class="item"
@@ -13,9 +13,20 @@
         {{ page }}
       </div>
     </div>
-    <div class="more item" @click="nextMoreClick" v-show="endIndex < datas.length">...</div>
-    <div class="last item" @click="lastClick" :class="{ active: activePage === lastData }">{{ lastData }}</div>
-    <div class="next item" @click="nextClick">&gt;</div>
+    <div class="more item" @click="nextMoreEvent" v-show="endIndex < datas.length">...</div>
+    <div class="last item" @click="lastEvent" :class="{ active: activePage === lastData }">{{ lastData }}</div>
+    <div class="next item" @click="nextEvent">&gt;</div>
+    <label for="search" class="search-label">Go to</label>
+    <input
+      id="search"
+      type="number"
+      title="搜索页码"
+      autocomplete="off"
+      :min="firstData"
+      :max="lastData"
+      :value="activePage"
+      @blur="searchBlurEvent"
+      @keydown="searchKeyupEvent" />
   </div>
 </template>
 
@@ -62,7 +73,31 @@ function setCurrentPage(currentPage: number) {
   emits("currentPage", activePage.value);
 }
 
-const prevClick = () => {
+function searchMethod(event: FocusEvent | KeyboardEvent) {
+  const target = event.target as HTMLInputElement;
+  const currentPage = Math.ceil(
+    Math.min(lastData, Math.max(firstData, Number(target.value)))
+  );
+
+  const leftPointer = currentPage - visibleCount / 2;
+  const rightPointer = currentPage + visibleCount / 2;
+
+  if (leftPointer <= firstData) {
+    startIndex.value = 0;
+    endIndex.value = startIndex.value + visibleCount;
+  } else if (rightPointer >= lastData) {
+    endIndex.value = datas.length;
+    startIndex.value = endIndex.value - visibleCount;
+  } else {
+    startIndex.value = leftPointer - 1;
+    endIndex.value = startIndex.value + visibleCount;
+  }
+
+  target.value = currentPage.toString();
+  setCurrentPage(currentPage);
+}
+
+const prevEvent = () => {
   const currentPage = activePage.value;
   if (currentPage >= firstData) {
     const leftPointer = visibleCount;
@@ -81,7 +116,7 @@ const prevClick = () => {
   setCurrentPage(activePage.value - 1);
 };
 
-const nextClick = () => {
+const nextEvent = () => {
   const currentPage = activePage.value;
   if (currentPage <= lastData) {
     const leftPointer = visibleCount;
@@ -105,7 +140,7 @@ const clickTarget = (event: MouseEvent) => {
   setCurrentPage(Number(target.textContent))
 };
 
-const prevMoreClick = () => {
+const prevMoreEvent = () => {
   startIndex.value = Math.max(0, startIndex.value - visibleCount);
   endIndex.value = startIndex.value + visibleCount;
   activePage.value -= activePage.value >= lastData ? visibleCount + 1 : visibleCount;
@@ -113,7 +148,7 @@ const prevMoreClick = () => {
   setCurrentPage(activePage.value);
 };
 
-const nextMoreClick = () => {
+const nextMoreEvent = () => {
   startIndex.value = Math.min(endIndex.value, datas.length - visibleCount);
   endIndex.value = startIndex.value + visibleCount;
   activePage.value += activePage.value <= firstData ? visibleCount + 1 : visibleCount;
@@ -121,19 +156,33 @@ const nextMoreClick = () => {
   setCurrentPage(activePage.value);
 };
 
-const firstClick = () => {
+const firstEvent = () => {
   startIndex.value = 0;
   endIndex.value = startIndex.value + visibleCount;
 
   setCurrentPage(firstData);
 };
 
-const lastClick = () => {
+const lastEvent = () => {
   startIndex.value = datas.length - visibleCount;
   endIndex.value = startIndex.value + visibleCount;
 
   setCurrentPage(lastData);
 };
+
+const searchBlurEvent = (event: FocusEvent) => {
+  searchMethod(event);
+}
+
+const searchKeyupEvent =  (event: KeyboardEvent) => {
+  switch (event.key) {
+    case 'Enter':
+      searchMethod(event);
+      break;
+    default:
+      break;
+  }
+}
 
 onMounted(() => {
     visibleCount = props.pagerCount - 2;
@@ -142,6 +191,7 @@ onMounted(() => {
 
     setCurrentPage(firstData);
 });
+
 </script>
 
 <style scoped>
@@ -174,6 +224,7 @@ onMounted(() => {
   height: inherit;
   line-height: 30px;
   cursor: pointer;
+  -webkit-user-select: none;
   user-select: none;
   border: 1px solid #000;
   box-sizing: border-box;
@@ -183,5 +234,33 @@ onMounted(() => {
 .active {
   background-color: #000;
   color: #fff;
+}
+
+.search-label[for="search"] {
+  margin-left: 20px;
+}
+
+#search {
+  padding: 0 4px;
+  width: 50px;
+  height: inherit;
+  text-align: center;
+  border-radius: 3px;
+  box-sizing: border-box;
+  border: 1px solid #000;
+  outline: none;
+  font-size: inherit;
+  transition: all 0.2s ease-in-out;
+  appearance: none;
+}
+
+#search:focus {
+  box-shadow: inset 0 0 1px .5px #000;
+  transition: all 0.2s ease-in-out;
+}
+
+#search::-webkit-outer-spin-button,
+#search::-webkit-inner-spin-button {
+    -webkit-appearance: none;
 }
 </style>
